1003f4e0fa6f009ab1d11fd4bff6923156f28976
mariancristea1002 <61822614+mariancristea1002@users.noreply.github.com>
2020-03-27 15:44:04 +0200

    Add files via upload

---
+++ L4/P3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+int main()
+
+{
+
+	int a, b, *aux;
+
+	aux = (int*)malloc(sizeof(int));
+
+	printf("a=");
+
+	scanf("%d", &a);
+
+	printf("b=");
+
+	scanf("%d", &b);
+
+	*aux = a;
+
+	a = b;
+
+	b = *aux;
+
+	printf("Dupa interschimb\na=%d si b=%d\n", a, b);
+
+	system("pause");
+
+	return 0;
